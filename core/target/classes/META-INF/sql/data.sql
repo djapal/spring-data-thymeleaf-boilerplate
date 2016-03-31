5a7577e998e7ae3ea9a9a4e0035b372d64c1022d
Apostolos Alexiadis <djapal@gmail.com>
2016-03-31 11:10:29 +0300

    add jetty plugin

---
+++ core/target/classes/META-INF/sql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO user VALUES (1, 'John',   'Smith');
+INSERT INTO user VALUES (2, 'Alex',   'Jones');
+INSERT INTO user VALUES (3, 'George', 'Clark');
+INSERT INTO user VALUES (4, 'Peter',  'Stevens');
